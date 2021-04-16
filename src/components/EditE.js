f5e39a301b9d3ffcfd562d9e15877f30363648f9
mahirkarim <mahirkarim@utexas.edu>
2021-04-16 08:20:17 -0500

    commented out edit e in case it breaks site

--- src/components/EditE.js
+++ src/components/EditE.js
@@ -1,91 +1,91 @@
-import React, { useState, useEffect } from "react";
-import { Grid, Paper } from "@material-ui/core";
-import { UseForm, Form } from "./UseForm";
-import Controls from "./controls/Controls";
+// import React, { useState, useEffect } from "react";
+// import { Grid, Paper } from "@material-ui/core";
+// import { UseForm, Form } from "./UseForm";
+// import Controls from "./controls/Controls";
 
-const initialFValues = {
-  id: 0,
-  evening: [],
-};
+// const initialFValues = {
+//   id: 0,
+//   evening: [],
+// };
 
-//have field to add new routine step and drop down to delete or edit, worry about inserting in the middle later
+// //have field to add new routine step and drop down to delete or edit, worry about inserting in the middle later
 
-export default function Edit() {
-  const [profile, setProfile] = useState([]);
-  const uid = localStorage.getItem("uid");
-  const { values, setValues, handleInputChange } = UseForm(initialFValues);
+// export default function Edit() {
+//   const [profile, setProfile] = useState([]);
+//   const uid = localStorage.getItem("uid");
+//   const { values, setValues, handleInputChange } = UseForm(initialFValues);
 
-  let req = JSON.stringify({ userID: uid });
-  fetch("http://vid.mergehealth.us/api/profile", {
-    method: "post",
-    body: req,
-    headers: { "Content-Type": "application/json" },
-  })
-    .then((res) => {
-      return res.json();
-    })
-    .then((json) => {
-      setProfile(json);
-      setValues({
-        username: json.username,
-        email: json.email,
-        city: json.city,
-        phone: json.phone,
-        birthday: json.birthday,
-        gender: json.gender,
-      });
-    });
+//   let req = JSON.stringify({ userID: uid });
+//   fetch("http://vid.mergehealth.us/api/profile", {
+//     method: "post",
+//     body: req,
+//     headers: { "Content-Type": "application/json" },
+//   })
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then((json) => {
+//       setProfile(json);
+//       setValues({
+//         username: json.username,
+//         email: json.email,
+//         city: json.city,
+//         phone: json.phone,
+//         birthday: json.birthday,
+//         gender: json.gender,
+//       });
+//     });
 
-  useEffect(() => {}, []); //the bracket calls use effect whenever what's in it changes
+//   useEffect(() => {}, []); //the bracket calls use effect whenever what's in it changes
 
-  return (
-    <Form>
-      <Grid container>
-        <Grid item xs={6}>
-          <Controls.Input
-            name="username"
-            label="username"
-            value={values.username}
-            onChange={handleInputChange}
-          />
-          <Controls.Input
-            label="Email"
-            name="email"
-            value={values.email}
-            onChange={handleInputChange}
-          />
-          <Controls.Input
-            label="Phone"
-            name="phone"
-            value={values.phone}
-            onChange={handleInputChange}
-          />
-          <Controls.Input
-            label="City"
-            name="city"
-            value={values.city}
-            onChange={handleInputChange}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <Controls.RadioGroup
-            name="gender"
-            label="Gender"
-            value={values.gender}
-            onChange={handleInputChange}
-            items={genderItems}
-          />
-          <Controls.DatePicker
-            name="birthday"
-            label="Birthday"
-            value={values.birthday}
-            onChange={handleInputChange}
-          />
-          <div>
-            <Controls.Button type="submit" text="Submit"></Controls.Button>
-          </div>
-        </Grid>
-      </Grid>
-    </Form>
-  );
-}
+//   return (
+//     <Form>
+//       <Grid container>
+//         <Grid item xs={6}>
+//           <Controls.Input
+//             name="username"
+//             label="username"
+//             value={values.username}
+//             onChange={handleInputChange}
+//           />
+//           <Controls.Input
+//             label="Email"
+//             name="email"
+//             value={values.email}
+//             onChange={handleInputChange}
+//           />
+//           <Controls.Input
+//             label="Phone"
+//             name="phone"
+//             value={values.phone}
+//             onChange={handleInputChange}
+//           />
+//           <Controls.Input
+//             label="City"
+//             name="city"
+//             value={values.city}
+//             onChange={handleInputChange}
+//           />
+//         </Grid>
+//         <Grid item xs={6}>
+//           <Controls.RadioGroup
+//             name="gender"
+//             label="Gender"
+//             value={values.gender}
+//             onChange={handleInputChange}
+//             items={genderItems}
+//           />
+//           <Controls.DatePicker
+//             name="birthday"
+//             label="Birthday"
+//             value={values.birthday}
+//             onChange={handleInputChange}
+//           />
+//           <div>
+//             <Controls.Button type="submit" text="Submit"></Controls.Button>
+//           </div>
+//         </Grid>
+//       </Grid>
+//     </Form>
+//   );
+// }
