5656a8f3a65b280a8443ece51486cad183a8da21
mahirkarim <32471172+mahirkarim@users.noreply.github.com>
2021-05-05 16:21:23 -0500

    Update FVid.js

--- src/components/FVid.js
+++ src/components/FVid.js
@@ -74,7 +74,7 @@ export default function FVid() {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
   const [frameKey, setFrameKey] = useState(0);
-  const [refresh, setRefresh] = useState(200000);
+  const [refresh, setRefresh] = useState(2000000);
 
   const profiles = () => {
     history.push("/profile");
